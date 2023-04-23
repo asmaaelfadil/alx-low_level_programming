283ca5b1ca3d82d4189deffa903e2d7da00bbdb2
Asmaa <asmaa.elfadil12@gmail.com>
2023-04-23 12:49:49 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,7 +15,7 @@ int main(void)
 	int m = 65;
 
 	while (n <= 122)
-	
+
 	{
 		putchar(n);
 		n++;
@@ -26,8 +26,8 @@ int main(void)
 	{
 		putchar(m);
 		m++;
-	}	
-	
+	}
+
 	putchar('\n');
 
 	return (0);
