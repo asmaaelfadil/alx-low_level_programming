ecd29aa61dbc30635c305d3a1e815a74422f2c94
Asmaa <asmaa.elfadil12@gmail.com>
2023-04-24 12:10:24 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 99; n++)
+	{
+	for (m = n; m <= 99; m++)
+	{
+
+	putchar(n / 10 + '0');
+	putchar(n % 10 + '0');
+
+	putchar(' ');
+	putchar(' ');
+
+	putchar(m / 10 + '0');
+	putchar(m % 10 + '0');
+	if (n != 99 || m != 99)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+
+	putchar('\n');
+	return (0);
+}
