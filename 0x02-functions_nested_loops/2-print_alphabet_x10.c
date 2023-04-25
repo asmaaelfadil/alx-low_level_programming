41e2eb50ed40157c91421c343f0cececf373c13d
Asmaa <asmaa.elfadil12@gmail.com>
2023-04-25 15:57:07 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -18,3 +18,15 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
