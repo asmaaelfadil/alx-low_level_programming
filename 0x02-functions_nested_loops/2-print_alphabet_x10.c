b62a1066180feb9698f8b8278b00a5efe7411556
Asmaa <asmaa.elfadil12@gmail.com>
2023-04-26 02:43:15 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        for (i = 0; i <= 10; i++)
-        {
-                for (j = 97; j <= 122; j++)
-                {
-                        _putchar(j);
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i <= 10; i++)
+	{
+	for (j = 97; j <= 122; j++)
+	{
+	 _putchar(j);
+	}
+
+	_putchar('\n');
+	}
 }
