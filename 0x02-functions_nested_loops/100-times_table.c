ddde8c66fdb2fe990be6b2311d951d61c1566a34
Asmaa <asmaa.elfadil12@gmail.com>
2023-04-26 18:07:03 -0700

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+	return;
+	}
+	for (int i = 0; i <= n; i++)
+	{
+	for (int j = 0; j <= n; j++)
+	{
+	printf("%d", i * j);
+	if (j < n)
+	{
+	printf(" , ");
+	}
+	}
+	printf("\n");
+	}
+}
