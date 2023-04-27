81ab1b0dd39fe66b1fb3f42e707967700f5c88e1
Asmaa <asmaa.elfadil12@gmail.com>
2023-04-27 16:19:32 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef main_h
+#define main_h
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,3 +11,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+#endif
