8a3dc657b1019b8c573b83f688c26306bf0208ff
Asmaa <asmaa.elfadil12@gmail.com>
2023-05-05 15:28:12 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,10 @@
 
 /**
  * _strcat - concatenates two strings
- *
  * @dest: input value
  * @src: input value
  *
- * Return : dest
- *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
