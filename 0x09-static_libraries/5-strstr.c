559ca3c43c231b6a84eaeb22040d637afca4fb20
Asmaa <asmaa.elfadil12@gmail.com>
2023-05-15 15:10:01 -0700

    libmy

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (haystack + i);
+	}
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x02. C - Functions, nested loops
+0x08. C - Recursion
