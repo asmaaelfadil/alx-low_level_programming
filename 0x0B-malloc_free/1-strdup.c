c9805100a7b947f72891bd297c3250d389c2105f
Asmaa <asmaa.elfadil12@gmail.com>
2023-05-16 16:50:31 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,25 @@
  */
 char *_strdup(char *str)
 {
+	int i = 0, size = 0;
+	char *m;
+
 	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
 	{
 	return (NULL);
 	}
-	char *duplicate = strdup(str);
-
-	return (duplicate);
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
 	}
+	return (m);
+}
