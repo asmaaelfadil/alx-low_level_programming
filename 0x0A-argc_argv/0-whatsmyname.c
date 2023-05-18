13f311b53903835bffb1e5e8783b2a23b68e7225
Asmaa <asmaa.elfadil12@gmail.com>
2023-05-17 17:51:57 -0700

    0. It aint what they call you, its what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Write a program that prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * REturn: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	if (argc >= 1)
