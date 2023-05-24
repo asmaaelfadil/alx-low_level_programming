48de30ee2c74b4700a398b9f51f7cf5a5e2a8efb
Asmaa <asmaa.elfadil12@gmail.com>
2023-05-23 17:48:02 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic information 
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: Define a new type struct dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner
+};
+#endif
